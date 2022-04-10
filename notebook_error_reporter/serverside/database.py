cdfd510e2072e3e7b582ad54dac29708cdc9b0c2
matteoferla <matteo.ferla@gmail.com>
2022-04-10 21:24:27 +0100

    :fire: a note about this being a dumpster fire

--- notebook_error_reporter/serverside/database.py
+++ notebook_error_reporter/serverside/database.py
@@ -1,3 +1,7 @@
+# this is a dumpster file.
+# copypaste from tutorial.
+# change to pyramid style:
+# https://github.com/matteoferla/MichelaNGLo-app/blob/master/michelanglo_app/models/__init__.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -6,8 +10,7 @@ from typing import Generator
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./error.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
