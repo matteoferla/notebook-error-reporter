2d14958524624428926bf04b2d70ad0c4a060af0
matteoferla <matteo.ferla@gmail.com>
2022-04-13 19:34:49 +0100

    :hammer: hack for older IPythons

--- notebook_error_reporter/error_event/error_event.py
+++ notebook_error_reporter/error_event/error_event.py
@@ -1,4 +1,9 @@
 from IPython.core.interactiveshell import InteractiveShell, ExecutionResult
+import IPython
+import warnings
+from types import TracebackType, MethodType
+
+
 
 
 class ErrorEvent:
@@ -15,12 +20,50 @@ class ErrorEvent:
                           execution_count=result.execution_count,
                           first_line=result.info.raw_cell.split('\n')[0])
 
+    def enable(self):
+        if IPython.version_info[0] > 5:
+            self.load_ipython_extension()
+        else:
+            self.monkeypatch_extension()
+
+
     def load_ipython_extension(self):
         """
         calling this method enables the error messaging by registering `self._post_run_cell`.
         """
         shell: InteractiveShell = get_ipython()  # noqa
+        assert IPython.version_info[0] < 6, 'Not applicable. use monkeypatch_extension'
         shell.events.register('post_run_cell', self._post_run_cell)
+
+    def monkeypatch_extension(self):
+        """
+        `load_ipython_extension` assumes that 'post_run_cell' event passes a ExecutionResult
+        object as happens in modern IPython.
+        Colab runs as of writing this on version 5.5 (`import IPython; IPython.version_info`),
+        whereas Jupyter on 8.2 on my machine.
+        This method compensates for it.
+        """
+        if IPython.version_info[0] > 5:
+            warnings.warn(f'Your IPython is modern {IPython.version_info}: `load_ipython_extension` is better.')
+        warnings.warn(f'Monkey patching approach used due to ancient IPython {IPython.version_info}')
+
+        def monkeypatch(shell_self, exc_tuple=None, *args, **kwargs):
+            try:
+                etype: type  # i.e. Exception class
+                value: Exception
+                tb: TracebackType
+                etype, value, tb = shell_self._get_exc_info(exc_tuple)
+                self.on_error(error=etype,
+                              execution_count=-1,
+                              first_line='Unknown')
+            except Exception:
+                pass
+            # the class still has a vanilla method.
+            return shell_self.__class__.showtraceback(shell_self, exc_tuple, *args, **kwargs)
+
+        shell: InteractiveShell = get_ipython()  # noqa
+        shell.showtraceback = MethodType(monkeypatch, shell)
+
 
     def on_error(self, *args, **kwargs):
         pass # virtual method to be overridden.
